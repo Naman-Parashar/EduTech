b7a8d3af504ce0805160ede5c6843f2fb532e7d3
Naman Parashar <namanparashar484@gmail.com>
2023-05-16 00:21:08 +0530

    completed faq section

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -2,4 +2,25 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
+});
+
+// Show or Hide FAQ's answer
+
+const faqs=document.querySelectorAll('.faq');
+//  to add event listeners for all of them.
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon=faq.querySelector('.faq_icon i');
+        if(icon.className === 'uil uil-plus')
+        {
+            icon.className= "uil uil-minus";
+        }
+        else{
+            icon.className= "uil uil-plus";
+        }
+    })
 })
+
